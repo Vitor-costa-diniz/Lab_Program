3355065ed1ad1f0a0ec42c57e00b1726ff22f62d
Vitor-costa-din <78705027+Vitor-costa-diniz@users.noreply.github.com>
2022-07-04 22:10:33 +0000

    Ponteiro com função, incompleto

---
+++ .ccls-cache/@home@runner@OrchidRuralDisks/Lista4@Ex8.c
@@ -0,0 +1,29 @@
+// Escreva um programa que busque um caracter fornecido em uma string utilizando ponteiros.
+
+#include <stdio.h>
+
+void compararString(char *lerString, char *verCaracter){
+  while(*lerString != '\0'){
+    if(*verCaracter == *lerString){
+      printf("caracter está contindo na string");
+      break;
+    }
+    else{
+      continue;
+    }
+    ++lerString;
+  }
+  *lerString = '\0';
+}
+
+int main(void){
+  char c[1];
+  char frase[60];
+  printf("Digite uma frase:\n");
+  scanf("%s",frase);
+
+  printf("Digite o caracter que deseja saber se está na string:\n");
+  scanf("%s",c);
+  compararString(frase,c);
+  return 0;
+}
