5fb1a5017063aaf11d72b9edb9205c43712ac487
Vitor-costa-din <78705027+Vitor-costa-diniz@users.noreply.github.com>
2022-07-04 20:32:26 +0000

    Ponteiro com Função

---
+++ .ccls-cache/@home@runner@OrchidRuralDisks/Lista4@Ex6.c
@@ -0,0 +1,22 @@
+// Escreva um programa que copie uma string para outra usando ponteiros.
+
+#include <stdio.h>
+void copiarString(char *copiarDaqui, char *colarAqui){
+  while(*copiarDaqui != '\0'){
+    *colarAqui = *copiarDaqui;
+    ++copiarDaqui;
+    ++colarAqui;
+  }
+  *colarAqui = '\0';
+}
+
+int main(void){
+  char *copiarDaqui, *colarAqui;
+  char string [] = "Pao com mortadela";
+  char string2[20];
+
+  copiarString(string,string2);
+  printf("%s\n",string2);
+
+  getchar();
+}
