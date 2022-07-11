e2a6895c8bbc04b7d42518c254fe014af64080f4
Vitor-costa-din <78705027+Vitor-costa-diniz@users.noreply.github.com>
2022-07-11 19:03:20 +0000

    Ex7

---
+++ .ccls-cache/@home@runner@OrchidRuralDisks/Lista3@Ex8.c
@@ -0,0 +1,1 @@
+// Fa ̧ca um programa em C que gere um vetor com 100 n ́umeros inteiros pseudoaleat ́orios no intervalodefinido pelo usu ́ario e apresente esse vetor ordenado (crescente). Fa ̧ca uma vers ̃ao com o m ́etodo de ordena ̧c ̃ao por sele ̧c ̃ao e outra pelo m ́etodo bolha (bubble sort).
