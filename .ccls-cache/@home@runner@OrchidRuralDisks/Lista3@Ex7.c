3b4719e1271ed06421841e16f6c49c612823dbc2
Vitor-costa-din <78705027+Vitor-costa-diniz@users.noreply.github.com>
2022-07-11 18:59:16 +0000

    Ex 7

--- .ccls-cache/@home@runner@OrchidRuralDisks/Lista3@Ex7.c
+++ .ccls-cache/@home@runner@OrchidRuralDisks/Lista3@Ex7.c
@@ -1,7 +1,24 @@
 // Fa ̧ca um programa em C que gere um vetor com 3 n ́umeros inteiros pseudoaleat ́orios no intervalo [0, 19] e apresente a sua m ́edia aritm ́etica e geom ́etrica.
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 #define TAM 3
 
 int main(){
+  char vet[TAM];
+  float soma;
+  double produto = 1,geo,expoente;
+  puts("Gerando números aleatórios");
+  for(int i=0; i<TAM;i++){
+    vet[i] = rand() %100 ;
+    printf("Número: %d\n",vet[i]);
+    soma = soma + vet[i];
+    produto = produto * vet[i];
+  }
   
+  expoente = 1.0 / 3.0 ;
+  geo = produto * expoente;
+  printf("A média aritimética dos números é: %.1f\n",soma/3);
+  printf("A média geométrica dos números é: %.1lf\n",geo);
+  return 0;
 }
