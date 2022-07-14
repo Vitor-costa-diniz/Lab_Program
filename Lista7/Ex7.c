5dd51658db0395b7c9fa6cd47a0ac6adafaf49f0
Vitor-costa-diniz <78705027+Vitor-costa-diniz@users.noreply.github.com>
2022-07-14 20:07:19 -0300

    Create Ex7.c

---
+++ Lista7/Ex7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+union Valores {
+  float a;
+  float b;
+};
+
+
+int main(void) {
+  union Valores N1;
+
+  N1.a = 150;
+  N1.b = 25;
+
+printf("Valor do membro a: %.2f\n", N1.a);
+printf("Valor do membro b: %.2f\n", N1.b);
+  
+  return 0;
+}
