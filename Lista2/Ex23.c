ed3eb835ff323fb6bdffbf3b7126d082f77aa3fd
Vitor-costa-diniz <78705027+Vitor-costa-diniz@users.noreply.github.com>
2022-07-14 18:50:15 -0300

    Create Ex23.c

---
+++ Lista2/Ex23.c
@@ -0,0 +1,14 @@
+/*Escreva um programa que mostre a diferen ̧ca entre pr ́e-incrementar e p ́os-incrementar usando o ope-
+rador –.*/
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int i = 0, k = 0;
+
+    printf("i:%d\n",i++);
+    
+    printf("k:%d\n",++k);
+    
+    return 0;
+}
