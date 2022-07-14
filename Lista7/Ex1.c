fb9cb074b60de329614966bbf974ecf3ddf6b75c
Vitor-costa-diniz <78705027+Vitor-costa-diniz@users.noreply.github.com>
2022-07-14 20:03:14 -0300

    Ex1.c

--- Lista7/Ex1.c
+++ Lista7/Ex1.c
@@ -1,1 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+  int X;
+  int Y;
+
+}node;
+
+
+int main(void) {
+
+  node *P_struct;
+
+  P_struct = malloc(sizeof(node));
+
+
+
+  P_struct->X = 142;
+  P_struct->Y = 115;
+
+
+  printf ("\nCampo X: %d // Campo Y: %d\n", P_struct->X, P_struct->Y);
+
+  
+
+  
+
+  
+  return 0;
+}
