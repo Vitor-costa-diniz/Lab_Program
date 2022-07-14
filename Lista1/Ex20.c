780c8dd5586c1d94e3e25d610b70dbf22393efe9
Vitor-costa-diniz <78705027+Vitor-costa-diniz@users.noreply.github.com>
2022-07-14 19:54:46 -0300

    Ex20

--- Lista1/Ex20.c
+++ Lista1/Ex20.c
@@ -1,5 +1,13 @@
-#include<stdio.h>
-int main(){
-  
-  return 0;
+#include <stdio.h>
+
+int main()
+{   
+    int num;
+
+    printf("Digite um numero: ");
+    scanf("%d", &num);
+
+    num % 2 == 0 ? printf("O numero digitado e par") : printf("O numero digitado e impar");
+
+    return 0;
 }
