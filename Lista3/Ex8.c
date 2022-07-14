f1b4f7e1b9dc6176ddd6c25caf919b6d3aa3660d
Vitor-costa-diniz <78705027+Vitor-costa-diniz@users.noreply.github.com>
2022-07-14 19:10:44 -0300

    Update Ex8.c

--- Lista3/Ex8.c
+++ Lista3/Ex8.c
@@ -1,1 +1,69 @@
-// Fa ̧ca um programa em C que gere um vetor com 100 n ́umeros inteiros pseudoaleat ́orios no intervalodefinido pelo usu ́ario e apresente esse vetor ordenado (crescente). Fa ̧ca uma vers ̃ao com o m ́etodo de ordena ̧c ̃ao por sele ̧c ̃ao e outra pelo m ́etodo bolha (bubble sort).
+/*8. Fa ̧ca um programa em C que gere um vetor com 100 n ́umeros inteiros pseudoaleat ́orios no intervalo
+definido pelo usu ́ario e apresente esse vetor ordenado (crescente). Fa ̧ca uma vers ̃ao com o m ́etodo
+de ordena ̧c ̃ao por sele ̧c ̃ao e outra pelo m ́etodo bolha (bubble sort).*/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define TAM 100
+
+void bubleSort(int *v, int intervalo);
+void selectSort(int *v, int intervalo);
+
+int main(int argc, char const *argv[])
+{
+    int intervalo, v[100];
+    puts("Insira um intervalo");
+    scanf("%d", &intervalo);
+    for (int i = 0; i < intervalo; i++)
+        v[i] = rand() % intervalo;
+
+    puts("Ordenado por bubble sort");
+    bubleSort(v, intervalo);
+    
+    puts("Ordenado por select sort");
+    selectSort(v, intervalo);
+
+    return 0;
+}
+
+void bubleSort(int *v, int intervalo)
+{
+    int aux = 0;
+    for (int i = 0; i < intervalo; i++)
+    {
+        if (v[i] > v[i + 1])
+        {
+            aux = v[i];
+            v[i] = v[i + 1];
+            v[i + 1] = aux;
+        }
+    }
+
+    for (int k = 0; k < intervalo; k++)
+    {
+        printf("%d", v[k]);
+    }
+}
+
+void selectSort(int *v, int intervalo)
+{
+    int aux;
+    for (int i = 0; i < intervalo - 1; i++)
+    {
+        for (int j = i + 1; j < intervalo; j++)
+        {
+            if (v[i] > v[j])
+            {
+                aux = v[i];
+                v[i] = v[j];
+                v[j] = aux;
+
+            }
+        }
+    }
+
+    for (int k = 0; k < intervalo; k++)
+    {
+        printf("%d", v[k]);
+    }
+}
